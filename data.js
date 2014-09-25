2dced973f8cbeb418a6b15e75adf8da28c42bf2f
forthwall <shubham.naik10@gmail.com>
2014-09-25 15:43:27 -0700

    this

--- data.js
+++ data.js
@@ -37,7 +37,7 @@ module.exports = function(app) {
 		"zxaqKEM5tAfnjcnWnLWXfEhdl",
 		"3ZNXXuW56UO1sl8S1NQh8jLd8RsSHw7IXvY6ABH6XjeJ3NfMmE",
 		"1.0",
-		"http://localhost:3030/auth/twitter/callback",
+		"http://huntlytics.servicethis.co/auth/twitter/callback",
 		"HMAC-SHA1"
 		);
 
